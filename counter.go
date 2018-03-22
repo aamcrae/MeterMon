6f4078bddb3b20b73bc81c01e7ab3a8f44a3c5be
Andrew McRae <andrew.a.mcrae@gmail.com>
2018-03-22 13:11:09 +1100

    More testing

--- counter.go
+++ counter.go
@@ -7,15 +7,19 @@ import (
     "time"
 )
 
-type Reading struct {
+// Accum contains the number of events counted during the interval,
+// as well as the start time of the interval.
+type Accum struct {
     start time.Time
     interval int
     count int32
 }
 
-func Counter(c chan Reading, interval int, fd *os.File) {
+// Counter accumulates the event counts during the intervals, and
+// sends the accumlated counts down a channel.
+func Counter(c chan Accum, interval int, fd *os.File) {
     var counter int32
-    var reading Reading
+    var reading Accum
     intv := time.Duration(interval) * time.Second  // Interval as duration.
     go countPulses(fd, &counter)
     for {
@@ -24,6 +28,11 @@ func Counter(c chan Reading, interval int, fd *os.File) {
         reading.start = now.Round(time.Second)
         // Calculate the duration to the end of the interval.
         togo := now.Add(intv).Truncate(intv).Sub(now)
+        // Ensure that at least a second will elapsed.
+        if (togo < time.Second/2) {
+            time.Sleep(time.Second/2)
+            continue
+        }
         time.Sleep(togo)
         reading.count = atomic.SwapInt32(&counter, 0)
         reading.interval = int(time.Now().Sub(now).Round(time.Second).Seconds())
@@ -31,6 +40,8 @@ func Counter(c chan Reading, interval int, fd *os.File) {
     }
 }
 
+// countPulses reads a file, and increments a counter for each
+// byte that is read.
 func countPulses(fd *os.File, counter *int32) {
     for {
         var b [1]byte
--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-    c := make(chan Reading, 50)
-    go Counter(c, 5, os.Stdin)
+    c := make(chan Accum, 50)
+    go Counter(c, 10, os.Stdin)
     for {
         r := <- c
-        fmt.Printf("Start: %v, intv %v count %v\n", r.start,
+        fmt.Printf("Start: %v, intv %v count %v\n", r.start.Unix(),
                    r.interval, r.count)
     }
 }
